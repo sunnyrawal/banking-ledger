073afc069db13753247ece71520e5bf6dd230702
shanirawal <shanirawal5@gmail.com>
2026-04-07 20:06:35 +0530

    test api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,6 +18,11 @@ const transactionRoute = require('./routes/transaction.route')
 /**
  * Use Routes
  */
+app.get('/' , (req,res) => {
+    res.json({
+        message : "Welcome to the banking API"
+    })
+})
 app.use('/api/auth' , authRoute)
 app.use('/api/accounts', accountRoute)
 app.use('/api/transactions', transactionRoute)
